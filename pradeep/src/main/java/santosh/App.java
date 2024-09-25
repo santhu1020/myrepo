d43e3428561644cfa084148d0b2544c16516d49e
santosh <santosh@gmail.com>
2024-09-25 19:48:12 +0530

    pradeep project commit

--- pradeep/src/main/java/santosh/App.java
+++ pradeep/src/main/java/santosh/App.java
@@ -1,12 +1,13 @@
 package santosh;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(proxyBeanMethods = false)
-public class App {
-
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
 }
