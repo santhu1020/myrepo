840b75325bac7a0ec5d7f350e821f7539a8a3b42
santosh <santosh@gmail.com>
2024-09-25 12:55:52 +0530

    pradeep project added to github

---
+++ pradeep/src/test/java/santosh/AppTest.java
@@ -0,0 +1,12 @@
+package santosh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
